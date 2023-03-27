0035e5b6a8533c4e38a14d64fbeea5da36c5c030
yuliya <yulya.s.n@bk.ru>
2023-03-27 20:20:16 +0300

    error test2

--- src/test/java/ru/netology/rest/CiPostmanEchoTest.java
+++ src/test/java/ru/netology/rest/CiPostmanEchoTest.java
@@ -26,12 +26,12 @@ public class CiPostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("привет")
+                .body("hello")
                 .when()
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет"))
+                .body("data", equalTo("hell"))
         ;
     }
 }
